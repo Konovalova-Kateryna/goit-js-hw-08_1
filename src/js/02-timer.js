f71c0cb51161cf0c442e11a56b8b8ae3f08ca2cd
Konovalova-Kateryna <kytana2112@gmail.com>
2022-06-26 22:17:58 +0300

    task2!!!

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,12 +4,13 @@ import Notiflix from 'notiflix';
 
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
-    fieldDays: document.querySelector('data-days'),
-    fieldHours: document.querySelector('data-hours'),
-    fieldMins: document.querySelector('data-minutes'),
-    fieldSecs: document.querySelector('data-seconds')
+    fieldDays: document.querySelector('span[data-days]'),
+    fieldHours: document.querySelector('span[data-hours]'),
+    fieldMins: document.querySelector('span[data-minutes]'),
+    fieldSecs: document.querySelector('span[data-seconds]')
 }
 const dateInput = document.querySelector('#datetime-picker');
+let currentDate = 0;
 
 refs.startBtn.setAttribute('disabled', true);
 
@@ -30,7 +31,7 @@ const options = {
     }
 };
 
-console.log(flatpickr(dateInput, options));
+flatpickr(dateInput, options);
 
 function pad(value) {
     return String(value).padStart(2, '0');
@@ -38,22 +39,27 @@ function pad(value) {
 
 const timer = {
     start() {
-            setInterval(() => {
+
+        const timerId = setInterval(() => {
+            const startTime = Date.now();
+            let deltaTime = currentDate - startTime;
+            const timeComponents = getTimeComponents(deltaTime);
             
-            const deltaTime = currentDate - Date.now();
-            const { days, hours, mins, secs } = getTimeComponents(deltaTime);
-                
-                // updateClockFace(deltaTime);
-                
-            console.log(`${days}:${hours}:${mins}:${secs}`);
-            
+            console.log(timeComponents);
+            updateClockFace(deltaTime);
+
+            if (deltaTime <= 0) {
+                clearInterval(timerId);
+                updateClockFace(0);
+        }
 
         }, 1000);
+        
     }
 };
 refs.startBtn.addEventListener('click', () => {
-    timer.start;
-    
+        timer.start();
+   
 });
 
 function getTimeComponents(time) {
@@ -63,22 +69,18 @@ function getTimeComponents(time) {
     const hour = min * 60;
     const day = hour * 24;
     
-    const days = Math.floor(time / day);
+    const days = pad(Math.floor(time / day));
     const hours = pad(Math.floor((time % day) / hour));
     const mins = pad(Math.floor(((time % day) % hour) / min));
     const secs = pad(Math.floor((((time % day) % hour) % min) / sec));
 
     return { days, hours, mins, secs };
 }
-function updateClockFace({ days, hours, mins, secs }) {
-    refs.fieldDays.innerHTML = `${days}`;
-    refs.fieldHours.textContent = `${hours}`;
-    refs.fieldMins.textContent = `${mins}`;
-    refs.fieldSecs.textContent = `${secs}`
+function updateClockFace(deltaTime) {
+    refs.fieldDays.textContent = getTimeComponents(deltaTime).days;
+    refs.fieldHours.textContent = getTimeComponents(deltaTime).hours;
+    refs.fieldMins.textContent = getTimeComponents(deltaTime).mins;
+    refs.fieldSecs.textContent = getTimeComponents(deltaTime).secs;
 
 }
 
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
